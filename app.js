13992e48eca19cf3b58e759d834746b3bdd7fb82
kjd3495 <92558372+kjd3495@users.noreply.github.com>
2022-02-03 17:44:08 +0900

    11th commit

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const text = document.querySelector(".text_inner");
 const blink = document.querySelector(".blink");
 const down = document.querySelector(".down");
 let i = 0;
+const content_box = document.querySelector(".content")
+const section = content_box.querySelectorAll("section");
+const nav_list = document.querySelector('.nav_right');
+const nav = nav_list.querySelectorAll('div');
 
 function typing(){
     if(i<content.length){
@@ -14,4 +18,54 @@ function typing(){
             },2000);
     }
 }
+
 setInterval(typing, 200);
+
+const option = {
+    root : null,
+    rootMargin : '0px',
+    threshold: 0.5,
+
+};
+const callback = (entries, observer) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+
+            switch(entry.target.id){
+                case "content__home": 
+                nav.forEach((nav)=> nav.style.cssText='transform:none; color:black;  border-bottom:0px;');
+                    nav[0].style.cssText='transform:scale(1.5); color:blue; border-bottom:1px solid black;';
+                    break;
+                case "content_about_1":
+                    nav.forEach((nav)=> nav.style.cssText='transform:none; color:black; border-bottom:0px; ');
+                    nav[1].style.cssText='transform:scale(1.5); color:blue; border-bottom:1px solid black;';
+                    break;
+                case"content_about_2":
+                    nav.forEach((nav)=> nav.style.cssText='transform:none; color:black; border-bottom:0px;');
+                    nav[1].style.cssText='transform:scale(1.5); color:blue; border-bottom:1px solid black;';
+                    break;
+                case "content__goal" :
+                    nav.forEach((nav)=> nav.style.cssText='transform:none; color:black; border-bottom:0px;');
+                    nav[2].style.cssText='transform:scale(1.5); color:blue; border-bottom:1px solid black;';
+                    break;
+                default: 
+                    nav.forEach((nav)=> nav.style.cssText='transform:none; color:black; border-bottom:0px;');
+                    break;
+            }
+        } else {
+            nav.forEach((nav)=> nav.style.cssText='transform:none; color:black; border-bottom:0px;');
+        }
+    });
+};
+
+const observer = new IntersectionObserver(callback, option);
+section.forEach((section)=>observer.observe(section));
+
+
+
+
+
+
+
+
+
